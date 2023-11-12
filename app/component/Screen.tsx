06ea66425be897fb4054f60d333c18886550e03d
mojtaba <110784968+gharibnaseri@users.noreply.github.com>
2023-11-11 22:30:12 -0500

    textfit

--- app/component/Screen.tsx
+++ app/component/Screen.tsx
@@ -1,16 +1,23 @@
-import { CalcContext } from "../context/CalContext"
-import { useContext } from "react"
+import { CalcContext } from "../context/CalContext";
+import { useContext } from "react";
+import { Textfit } from 'react-textfit';
 
 const Screen = () => {
-  const {calc} = useContext(CalcContext)
+  const { calc } = useContext(CalcContext);
   console.log(calc);
   
-  
-  return (
-    <div className="h-8 mb-80">
-      
-1234    </div>
-  )
-}
 
-export default Screen
+  return (
+    <Textfit
+        mode="multiple"
+        max={100}
+        forceSingleModeWidth={true}>
+          2131232122131232132213
+        {calc.num? calc.num : calc.resç}
+      </Textfit>
+    // <Textfit mode="single" className="h-17 mt-8">867</Textfit>
+  );
+};
+
+export default Screen;
+// {calc.num? calc.num : calc.resç}
